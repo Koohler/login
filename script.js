ee64e4c8df71bb26bc66de3151235d39e279b994
Koohler <90351653+Koohler@users.noreply.github.com>
2021-11-11 13:26:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function logar(){
+    var usuario = document.getElementById("usuario");
+    var senha = document.getElementById("senha");
+  console.log("Usuário: " + usuario.value+" Senha:" + senha.value);
+  if(usuario.value == "Carlos1" && senha.value == "123456"){
+        alert("Usuário autenticado!");
+    }
+    else{
+        alert("Usuário ou senha Invalidos!");
+    }
+  if(usuario.value == "Carlos1" && senha.value == "123456"){
+        alert("Usuário autenticado!");
+    }
+    else{
+        alert("Usuário ou senha Invalidos!");
+    }
